1b8c4fa9f69093eecc65309eabe42ef33b5b707b
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2021-07-30 16:23:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var menu_list_array= ["Veg Mergherita Pizza", "Chicken Pizza", "Mac and Cheese pizza"];
+console.log(menu_list_array);
+document.getElementById("display_menu").innerHTML = "";
+function add_item(){
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+}
